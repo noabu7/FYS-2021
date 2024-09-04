7e64ced49b004c3ddf13812b702d3dd417662b77
Nour El-Din Abu-Alam <noabu7034@uit.no>
2024-09-04 12:41:33 +0200

    Update music_clasifier.py

--- music_clasifier.py
+++ music_clasifier.py
@@ -166,7 +166,3 @@ def compute_confusion_matrix(test_features, test_labels, weights, bias):
 
 confusion_matrix = compute_confusion_matrix(test_features, test_labels, weights, bias)
 print(f"Confusion matrix:\n {confusion_matrix}")
-
-
-
-#This is a test for github :)
